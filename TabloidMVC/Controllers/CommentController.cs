06b5b95304d92680e96759e0e3c0ce18e4940377
Sisi Freeley <liusi33@gmail.com>
2020-09-23 14:44:47 -0500

    comments list showing up for individual post

---
+++ TabloidMVC/Controllers/CommentController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ICommentRepository _commentRepository;
+        private readonly IPostRepository _postRepository;
+
+        public CommentController(ICommentRepository commentRepository, IPostRepository postRepository)
+        {
+            _commentRepository = commentRepository;
+            _postRepository = postRepository;
+        }
+        // GET: CommentsController
+        public ActionResult Index(int id)
+        {
+
+            List<Comment> comments = _commentRepository.GetAllCommentsByPostId(id);
+            return View(comments);
+        }
+
+  
+        // GET: CommentsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CommentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CommentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CommentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/CommentRepository.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class CommentRepository : BaseRepository, ICommentRepository
+    {
+        public CommentRepository(IConfiguration config) : base(config) { }
+
+        public List<Comment> GetAllCommentsByPostId(int postId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                      SELECT c.Id, c.PostId, c.UserProfileId, c.[Subject], c.Content, c.CreateDateTime, p.Title, u.DisplayName
+                        FROM Comment c
+                        LEFT JOIN Post p 
+                        ON c.PostId = p.Id
+                        LEFT JOIN UserProfile u
+                        ON c.UserProfileId = u.Id
+                        WHERE c.PostId = @id
+                        ORDER BY CreateDateTime DESC
+                       ";
+                    cmd.Parameters.AddWithValue("@id", postId);
+                    
+                    var reader = cmd.ExecuteReader();
+
+                    var comments = new List<Comment>();
+
+                    while (reader.Read())
+                    {
+                        Comment comment = new Comment
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            PostId = reader.GetInt32(reader.GetOrdinal("PostId")),
+                            UserProfileId = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                            Subject = reader.GetString(reader.GetOrdinal("Subject")),
+                            Content = reader.GetString(reader.GetOrdinal("Content")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                            Post = new Post
+                            { 
+                                Id = reader.GetInt32(reader.GetOrdinal("PostId")),
+                                Title = reader.GetString(reader.GetOrdinal("Title"))
+                            },
+                            UserProfile = new UserProfile
+                            { 
+                                Id = reader.GetInt32(reader.GetOrdinal("UserProfileId")),
+                                DisplayName = reader.GetString(reader.GetOrdinal("DisplayName"))
+                            }
+                        };
+
+                        comments.Add(comment);
+                       
+                    }
+
+                    reader.Close();
+
+                    return comments;
+                }
+            }
+        }
+    }
+}
