f10218b80f843ca931f2a86369373e73bf2d1ca7
Kelley Crittenden <kelleylcrittenden@gmail.com>
2020-09-24 13:43:03 -0500

    Approving Branch

--- TabloidMVC/Repositories/IUserProfileRepository.cs
+++ TabloidMVC/Repositories/IUserProfileRepository.cs
@@ -6,10 +6,6 @@ namespace TabloidMVC.Repositories
     public interface IUserProfileRepository
     {
         UserProfile GetByEmail(string email);
-<<<<<<< HEAD
-
-=======
         List<UserProfile> GetAllUserProfiles();
->>>>>>> 2a8dbbab39e32c19744cb42f1122d454eadef585
     }
 }
