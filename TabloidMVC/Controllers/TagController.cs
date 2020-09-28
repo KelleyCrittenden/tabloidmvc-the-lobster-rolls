acc38871daa92cb65c7034a2c3b37406f92b7fa0
Kelley Crittenden <kelleylcrittenden@gmail.com>
2020-09-28 10:42:28 -0500

    Authorization in place

--- TabloidMVC/Controllers/TagController.cs
+++ TabloidMVC/Controllers/TagController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using TabloidMVC.Models;
 using TabloidMVC.Repositories;
 
 namespace TabloidMVC.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class TagController : Controller
     {
         private readonly ITagRepository _tagRepository;
