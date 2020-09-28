3238d12bd5926271877b9b61b68204d8f808ccf3
Tyler Grey Hilliard <tylerghilliard@gmail.com>
2020-09-28 11:30:20 -0500

    users can no longer type in the url to get to edit and delete if the post is not theirs

--- TabloidMVC/Controllers/PostController.cs
+++ TabloidMVC/Controllers/PostController.cs
@@ -8,6 +8,7 @@ using TabloidMVC.Repositories;
 using TabloidMVC.Models;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 namespace TabloidMVC.Controllers
 {
@@ -106,8 +107,15 @@ namespace TabloidMVC.Controllers
                 Post = post,
                 CategoryOptions = categories
             };
-
-            return View(vm);
+            if (post == null || post.UserProfileId != int.Parse(User.Claims.ElementAt(0).Value))
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(vm);
+            }
+            
         }
 
         [HttpPost]
@@ -142,8 +150,16 @@ namespace TabloidMVC.Controllers
         {
             Post post = _postRepository.GetPublishedPostById(id);
 
-            return View(post);
+            if (post == null || post.UserProfileId != int.Parse(User.Claims.ElementAt(0).Value))
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(post);
+            }
         }
+           
 
         [HttpPost]
         public IActionResult Delete(Post post)
